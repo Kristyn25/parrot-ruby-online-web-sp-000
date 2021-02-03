2aafa91d61381f71ef80e2177c051447bb526672
Kristyn Little <kristynlittle@frontier.com>
2021-02-03 01:19:36 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts "Squawk!"
-  "Squawk!"
+def parrot_sound
+  parrot = "Squawk!"
+  puts parrot
+  return parrot
 end
